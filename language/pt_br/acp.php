8ec5a0cfd8695f7be6f9565f36627658418ef149
Jakub Senko <jakubsenko@gmail.com>
2019-06-19 07:56:28 +0200

    Add pt_br translation

---
+++ language/pt_br/acp.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Extension News. An extension for the phpBB Forum Software package.
+ * Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.0.0] (https://github.com/phpBBTraducoes)
+ * @copyright (c) 2019, Jakub Senko
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'EXT_NEWS'	=> 'As extensões atualizadas recentemente',
+));
